29dcbffd5594932382ee908e79d3ff6e134c040b
ipcara <18ipc90@millshighsch.org>
2016-08-29 08:34:08 -0700

    first program

---
+++ src/Hello_world.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Cara
+ * @version 08/29/16
+ */
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("Hello World! From Cara");
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Cara
+ *@version 08/29/16
+ */
+public class Hello_world {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("Hello World! From Cara");
+
+
+	}
+
+}
