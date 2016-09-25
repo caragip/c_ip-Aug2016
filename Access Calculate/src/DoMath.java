e37f22b611b6eb0d0018ee3c0e683fd863bbcb94
Cara Ip <caraip123@gmail.com>
2016-09-25 00:27:02 -0700

    Finished Calculate Library!

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -3,16 +3,13 @@
  */
 
 /**
- * @author APCS1
- *
+ * @author Cara Ip
+ * DoMath to run the Calculate functions. 
+ * September 24, 2016
  */
 public class DoMath {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		double answer = Calculate.exponent(2,3);
+		int answer = Calculate.square(2);
 		System.out.println(answer);
 		
 
