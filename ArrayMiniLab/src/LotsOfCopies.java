4fe81c6b1d580a97bfdb036f985a03bc79be7837
Cara Ip <caraip123@gmail.com>
2016-10-31 08:19:23 -0700

    Array Mini Lab

---
+++ ArrayMiniLab/src/LotsOfCopies.java
@@ -0,0 +1,14 @@
+
+public class LotsOfCopies {
+	public static void main(String[] args) {
+		int num = 7;
+		String strMain = "APCS";
+		int [] arrMain = {1,2,3,4,5};
+		System.out.println(num);
+		System.out.println(strMain);
+		System.out.println(arrMain);
+	}
+	public static void changeMe(int x, String str, int [] arr) {
+	}
+} 
+
